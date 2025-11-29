26970ad520779074d0e801d6562fe336bb3c0952
SobanHM <sobanhussainmahesar@gmail.com>
2025-11-29 15:55:57 +0500

    Balanced dataset for all classe.Applied data augmentations and compared results before and after data augmentation

--- balance_and_report.py
+++ balance_and_report.py
@@ -1,14 +1,13 @@
 from dataset_balancer import DatasetBalancer
 
 DATASET = r"C:/Users/soban/PycharmProjects/Smart-Toll-Tax-System/Merged_Dataset"
-CLASSES = ["car", "truck", "van", "bus"]
 
-db = DatasetBalancer(DATASET, CLASSES)
+# classes read from 'data.yaml' inside the Merged_Dataset
+db = DatasetBalancer(DATASET)
 
-print("\n BEFORE BALANCING:")
+print("\n Before Balancing:")
 db.print_report()
 
 db.balance_dataset(output_multiplier=1.5)
-
-print("\n AFTER BALANCING:")
+print("\n After Balancing:")
 db.print_report()
