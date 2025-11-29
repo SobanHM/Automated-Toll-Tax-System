a240a259ccab8993ccef4b405ffb922ec7377e8b
SobanHM <sobanhussainmahesar@gmail.com>
2025-11-29 20:27:13 +0500

    Updated: added Yolo weights and tracker to config

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ TOLL_RATES = {
     "van": 100,
     "bus": 150
 }
+
+path_best_model = r"C:\Users\soban\PycharmProjects\Smart-Toll-Tax-System\Merged_Dataset\runs\train\yolov8\best.pt"
+YOLO_WEIGHTS = path_best_model
+TRACKER_CONFIG = "botsort.yaml"
